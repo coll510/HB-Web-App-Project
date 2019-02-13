e56cb7a3c14d631147edf9c69d561ee52489964c
Key Doby <krdoby@yahoo.com>
2019-02-13 03:43:13 +0000

    Updated search results

--- server.py
+++ server.py
@@ -6,19 +6,22 @@ import os
 import requests
 from flask import Flask, render_template, request, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
+#import ipdb 
 
 
 app = Flask(__name__)
-# app.secret_key = "secretsecretsecret" 
+app.secret_key = "secretsecretsecret" 
 # #This pulls from my os so connect this to my secrets.sh file.
 
-# EVENTBRITE_TOKEN = os.environ.get('#')
+EVENTBRITE_TOKEN = os.environ.get('EVENTBRITE_TOKEN')
 # #put variable name of my secrets file token
 
-# EVENTBRITE_URL = 
+EVENTBRITE_URL = "https://www.eventbriteapi.com/v3/"
 # #get url from eventbrite
-# USER_ID = 
+USER_ID = os.environ.get('EVENTBRITE_USER_ID')
 #my eventbrite user id
+
+AUTH_HEADER = {'Authorization': 'Bearer ' + EVENTBRITE_TOKEN}
 
 # i will need to parse the json data that I get from eventbrite's api, set a
 #variable or variables that I want to use for the data, and send it to a diff
@@ -37,6 +40,68 @@ def show_classsearch_form():
     """Show dance class search form."""
 
     return render_template("danceclass-search.html")
+
+@app.route("/search-results")
+def find_danceclasses():
+    """Search for and display dance class results from API."""
+
+    #ipdb.set_trace()
+    location = request.args.get("city")
+    #print(dir(requests))
+    #yellow/city is the name in my html file
+    distance = request.args.get("distance")
+    sort = request.args.get("sort")
+    style = request.args.get("style")
+    distance = distance + "mi"
+
+    if location and distance:
+
+        #distance = distance 
+
+        payload = {"q": "dance class", 
+                    "location.address" : location, 
+                    "location.within" : distance,
+                    "sort_by" : sort
+        #yellow - api params to get
+
+        }
+
+    elif location and style:
+        # search by dance style in the query 
+        payload = {"q": f"dance class {style}", 
+                    "location.address": location,
+                    "sort_by": sort
+
+        }
+
+    else:
+        #general query search of "dance class"
+        payload = {"q": "dance class",
+                    "location.address": location,
+                    "sort_by": sort 
+                    }
+
+
+    response = requests.get(EVENTBRITE_URL + "events/search", 
+                            params = payload, 
+                            headers=AUTH_HEADER)
+
+    data = response.json()
+
+    if response.ok:
+        classes = data['events']
+
+    else: 
+        flash(f"No classes: {data['error_description']}")
+        classes = []
+
+    return render_template("/search-results.html", 
+                            data=pformat(data),
+                            results=classes)
+
+# else: 
+#     flash("Please complete the required information.")
+#     return redirect("/danceclass-search")
 
 
 
@@ -60,7 +125,7 @@ if __name__ == "__main__":
 
     #connect_to_db(app)
 
-    # Use the DebugToolbar
-    # DebugToolbarExtension(app)
+    #Use the DebugToolbar
+    DebugToolbarExtension(app)
 
     app.run(host="0.0.0.0")
